323b7543f7ff4e7a38a8a24233f09eeefcf91dd5
ChevasP <chevasx40@gmail.com>
2022-01-21 13:47:07 +0100

    form1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:fixhome_ready/src/pages/home_page.dart';
-import 'package:fixhome_ready/src/theme/app_theme.dart';
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -7,26 +7,23 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _setupMode(),
-        builder: (context, snapshot) {
-          return SizedBox.square(
-              dimension: 50.0, child: CircularProgressIndicator());
-        });
+    return MaterialApp(
+      title: 'FixHome',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
   }
 }
 
-Future<bool> _setupMode() async {
+_setupMode() async {
   final SharedPreferences prefs = await SharedPreferences.getInstance();
   bool mode = prefs.getBool("mode") ?? true;
   return mode;
--- lib/src/widgets/add_establishment_widget.dart
+++ lib/src/widgets/add_establishment_widget.dart
@@ -1,172 +1,85 @@
 import 'package:flutter/material.dart';
 
-class AddEstablishmentWidget extends StatelessWidget {
-  GlobalKey<FormState> keyForm = GlobalKey();
-  TextEditingController nameCtrl = TextEditingController();
-  TextEditingController descriptionCtrl = TextEditingController();
-  TextEditingController identificationCtrl = TextEditingController();
-  TextEditingController passwordCtrl = TextEditingController();
-  TextEditingController repeatPassCtrl = TextEditingController();
+class AddEstablishmentWidget extends StatefulWidget {
   AddEstablishmentWidget({Key? key}) : super(key: key);
+
+  @override
+  _AddEstablishmentWidgetState createState() => _AddEstablishmentWidgetState();
+}
+
+class _AddEstablishmentWidgetState extends State<AddEstablishmentWidget> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Añadir Establecimiento'),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          margin: const EdgeInsets.all(60.0),
-          child: Form(
-            key: keyForm,
-            child: formUser(),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            margin:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 14.0),
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            decoration: BoxDecoration(
+                color: Theme.of(context).scaffoldBackgroundColor,
+                borderRadius: BorderRadius.circular(10.0),
+                border: Border.all(
+                    width: 2.0, color: Theme.of(context).primaryColorDark)),
+            child: Form(
+                key: _formKey,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 14.0, horizontal: 7.0),
+                  child: Column(children: [
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.only(top: 7.0),
+                        child: Text("Ingresar la imagen ",
+                            style: Theme.of(context).textTheme.subtitle1)),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: []),
+                    TextFormField(
+                        keyboardType: TextInputType.text,
+                        decoration:
+                            const InputDecoration(labelText: "Establecimiento"),
+                        maxLength: 150,
+                        maxLines: 4),
+                    TextFormField(
+                        keyboardType: TextInputType.text,
+                        decoration:
+                            const InputDecoration(labelText: "Descripcion"),
+                        maxLength: 150,
+                        maxLines: 4),
+                    TextFormField(
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                            labelText: "Calificacion del 1 al 5"),
+                        maxLength: 1),
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      decoration:
+                          const InputDecoration(labelText: "Nombre del Dueño"),
+                      maxLength: 50,
+                    ),
+                    TextFormField(
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                            labelText: "Número de Contacto del dueño: "),
+                        maxLength: 150,
+                        maxLines: 4),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20.0),
+                      child: ElevatedButton.icon(
+                          onPressed: () {},
+                          label: const Text("Guardar"),
+                          icon: const Icon(Icons.save)),
+                    )
+                  ]),
+                )),
           ),
-        ),
+        ],
       ),
     );
-  }
-
-  formItemsDesign(icon, item) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 7),
-      child: Card(child: ListTile(leading: Icon(icon), title: item)),
-    );
-  }
-
-  Widget formUser() {
-    return Column(
-      children: <Widget>[
-        formItemsDesign(
-            Icons.business,
-            TextFormField(
-              controller: nameCtrl,
-              decoration: const InputDecoration(
-                labelText: 'Nombre',
-              ),
-              validator: validateName,
-            )),
-        formItemsDesign(
-            Icons.receipt,
-            TextFormField(
-              controller: descriptionCtrl,
-              decoration: const InputDecoration(
-                labelText: 'Descripción',
-              ),
-              validator: validateName,
-            )),
-        formItemsDesign(
-            Icons.person,
-            TextFormField(
-              controller: identificationCtrl,
-              decoration: const InputDecoration(
-                labelText: 'Dueño',
-              ),
-              validator: validateName,
-            )),
-        formItemsDesign(
-            Icons.date_range,
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Telefono',
-              ),
-              keyboardType: TextInputType.phone,
-              maxLength: 10,
-              validator: validateIndentification,
-            )),
-        formItemsDesign(
-            Icons.email,
-            TextFormField(
-              controller: descriptionCtrl,
-              decoration: new InputDecoration(
-                labelText: 'Calificación de 1 a 5',
-              ),
-              keyboardType: TextInputType.number,
-              maxLength: 1,
-              validator: qualification,
-            )),
-        GestureDetector(
-            onTap: () {
-              save();
-            },
-            child: Container(
-              margin: const EdgeInsets.all(30.0),
-              alignment: Alignment.center,
-              decoration: ShapeDecoration(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0)),
-                gradient: const LinearGradient(colors: [
-                  Color(0xFF0EDED2),
-                  Color(0xFF03A0FE),
-                ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-              ),
-              child: const Text("Guardar",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500)),
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
-            ))
-      ],
-    );
-  }
-
-  String? validateName(String? value) {
-    String pattern = r'(^[a-zA-Z ]*$)';
-    RegExp regExp = RegExp(pattern);
-    if (value!.length == 0) {
-      return "El nombre es necesario";
-    } else if (!regExp.hasMatch(value)) {
-      return "El nombre debe de ser a-z y A-Z";
-    }
-    return null;
-  }
-
-  String? validateIndentification(String? value) {
-    String patttern = r'(^[0-9]*$)';
-    RegExp regExp = RegExp(patttern);
-    if (value!.length == 0) {
-      return "La cedula  es necesaria";
-    }
-    return null;
-  }
-
-  String? validateEmail(String? value) {
-    String pattern =
-        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-    RegExp regExp = RegExp(pattern);
-    if (value!.length == 0) {
-      return "El correo es necesario";
-    } else if (!regExp.hasMatch(value)) {
-      return "Correo invalido";
-    } else {
-      return null;
-    }
-  }
-
-  String? validatePassword(String? value) {
-    print("valorrr $value passsword ${passwordCtrl.text}");
-    if (value != passwordCtrl.text) {
-      return "Las contraseñas no coinciden";
-    }
-    return null;
-  }
-
-  void save() {
-    if (keyForm.currentState!.validate()) {
-      print("Nombre ${nameCtrl.text}");
-      print("Cedula ${identificationCtrl.text}");
-      print("Correo ${descriptionCtrl.text}");
-      keyForm.currentState!.reset();
-    }
-  }
-
-  String? qualification(String? value) {
-    var val1 = int.parse(value!);
-    if (val1 > 5 && val1 < 1) {
-      return "La calificación va de 1 a 5";
-    } else {
-      return null;
-    }
   }
 }
